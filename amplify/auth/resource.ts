101c762d18d88741f936652b853755f9b923de46
unknown <madinamir21@github.com>
2026-01-28 12:33:30 -0500

    updated authentication in resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,11 @@
-import { defineAuth } from '@aws-amplify/backend';
-
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
+import { defineAuth } from "@aws-amplify/backend";
+export const auth = defineAuth({ 
+  loginWith: { 
+    email: { 
+      verificationEmailStyle: "CODE", 
+      verificationEmailSubject: "Welcome to Super Awesome Shopper Helper!", 
+      verificationEmailBody: (createCode) => 
+        `Use this code to confirm your account: ${createCode()}`, 
+    }, 
   },
 });

982ce4123c1dec87270a3520a6d5da4f21e3ffeb
Jason Zhang <jzhan111@hydra12.eecs.utk.edu>
2026-01-28 14:52:45 -0500

    update bedrock.js

--- amplify/data/bedrock.js
+++ amplify/data/bedrock.js
@@ -1,1 +1,24 @@
 /* backend */
+export function request(ctx) {
+    const { input } = ctx.args;
+  
+    return {
+      resourcePath: `/model/openai.gpt-oss-20b-1:0/invoke`,
+      method: "POST",
+      params: {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          max_tokens: 1000,
+          temperature: 0.7,
+          messages: [
+            {
+              role: "user",
+              content: input
+            }
+          ]
+        }),
+      },
+    };
+  }

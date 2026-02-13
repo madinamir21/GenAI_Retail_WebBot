58df79cd8a4b7176c005b6d7f2d00c7a306797ef
Jackson Stern <jediknight10101@gmail.com>
2026-02-13 14:38:37 -0500

    Backend setup

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,8 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "genairetailwebbot": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  }
+}
